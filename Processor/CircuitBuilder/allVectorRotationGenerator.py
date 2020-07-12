9b1b4d1de5c0ceba978fae5d556194c7c34ca7ac
Julien Giraud <giraud740@gmail.com>
2020-07-12 18:59:22 +0200

    Valeurs plus cohérentes

--- Processor/CircuitBuilder/allVectorRotationGenerator.py
+++ Processor/CircuitBuilder/allVectorRotationGenerator.py
@@ -1,20 +1,6 @@
 import json
 from numpy import array
 
-def getFaceName(u):
-    if (u[0] == -2):
-        return 'N'
-    if (u[0] == 2):
-        return 'J'
-    if (u[1] == -2):
-        return 'R'
-    if (u[1] == 2):
-        return 'O'
-    if (u[2] == -2):
-        return 'B'
-    if (u[2] == 2):
-        return 'V'
-
 if __name__ == "__main__" :
 
     file=True
@@ -27,28 +13,25 @@ if __name__ == "__main__" :
     aretes = generation['aretes']
     transition = solutions['transition']
     faces = transition['faces']
+    moveToFace = transition['move-to-face']
 
     moves = ["R", "R'", "U", "U'", "L", "L'", "F", "F'", "D", "D'", "B", "B'"]
     stickerType = ['A', 'C']
 
     for st in stickerType:
-        print('StickerType :', st)
-        for i in range(1, 25):
-            print('    Number :', i)
-            for move in moves:
-                print('        Move :', move)
+        for move in moves:
+            for i in range(1, 25):
                 key = str(i) + move
                 sol = solutions[st]
                 val = sol[key] if key in sol else i
-                print('        Key :', key)
-                print('        Val :', val)
                 u = transition[st][str(i)]
-                v = faces[getFaceName(u)]
+                v = moveToFace[move]
                 w = transition[st][str(val)]
-                recap = '(' + str(u[0]) + ', ' + str(u[1]) + ', ' + str(u[2]) + ')\n      + '
-                recap += '(' + str(v[0]) + ', ' + str(v[1]) + ', ' + str(v[2]) + ')\n      = '
-                recap += '(' + str(w[0]) + ', ' + str(w[1]) + ', ' + str(w[2]) + ')'
-                print('        Recap :\n       ', recap)
+
+                print('        Key+value :', st, key, '->', val)
+                print('        Sticker :', u)
+                print('        Move :   ', v)
+                print('        Result : ', w)
                 print('')
             print('')
         print('')
